80a3b54949a51039fa1a487cbb5dbf9a7ce0b235
Jeromemilk <59964908+Jerome-Lalnunfima@users.noreply.github.com>
2020-03-23 10:12:03 +0530

    s
    s

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Foodle</title>
+    <link rel="stylesheet" href="./assets/css/main.css">
+</head>
+<body>
+    <div class="header">
+        <div class="upper">
+            <div class="logo">
+                <img src="#" alt="">
+            </div>
+            <div class="social">
+                <span>fb</span>
+                <span>insta</span>
+            </div>
+            <div class="clearfix"></div>
+        </div>
+        <div class="lower">
+            <ul class="nav-links">
+                <li>
+                    <a href="">Home</a>
+                </li>
+                <li>
+                    <a href="profile.php">Profile</a>
+                </li>
+                <li><a href="">Login</a></li>
+                <li><a href="">Post</a></li>
+                <li><a href="">Contact Us</a></li>
+                <li><a href="about.php">About Us</a></li>
+            </ul>
+            <div class="search">
+                <form action="">
+                    <label for="search">Search:</label>
+                    <input placeholder="Search here" id="search" type="text" name="search">
+                    <button type="submit">Go</button>
+                </form>
+            </div>
+        </div>
+       
+        <div class="clearfix"></div>
+    </div>
+    <div class="main-content">
+        <div class="sidebar">
+            <div class="rect">
+                 <h5>Price range</h5>
+                 <div class="min-max">
+                     <div class="min">
+                         <label>Min:</label>
+                         <input type="text">
+                     </div>
+                     <div class="max">
+                         <label>Max:</label>
+                         <input type="text">
+                     </div>
+                 </div>
+                 <div class="butt">
+                     <button>Go</button>
+                        </div>
+            </div>
+        </div>
+        <div class="main">
+            <div class="rect1"> 
+                
+                    <div class="img" >
+                        <img src="./assets/img/download(1).jpg" alt="Test">
+                    </div>
+            </div>
+        </div>
+    </div>
+    <script src="./assets/js/main.js"></script>
+</body>
+</html>
